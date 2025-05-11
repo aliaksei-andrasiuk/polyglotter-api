2854c5ea3ad2596f98dbd1125fae0b27fadfbe99
Aliaksei Andrasiuk <aliaksei.andrasiuk.business@gmail.com>
2025-05-12 00:01:25 +0200

    fix dataset endpoint

--- src/routes/datasets.route.ts
+++ src/routes/datasets.route.ts
@@ -5,6 +5,8 @@ import { prisma } from "../services";
 export const getDataset: RequestHandler = async (_, res, next) => {
     try {
         const dataset = await prisma.phraseMapping.findMany();
+        console.log(dataset);
+
 
         res.status(200).json({
             status: 200,
